2765a1b48871f16d9e9b7c3c0e34838aa3f7a143
Sinclert <Sinclert@hotmail.com>
2018-04-05 19:20:52 +0200

    Creating 'get_next-page' and 'build-url' funcs

--- src/image_miner.py
+++ src/image_miner.py
@@ -2,16 +2,16 @@
 
 
 import base64
-import bs4
 import io
 import requests
 
+from bs4 import BeautifulSoup
 from PIL import Image
 
 
 
 
-def get_images_page(domain, query, **parameters):
+def build_url(domain, path, parameters = None):
 
 	""" Search for the query in a given domain and returns the html object
 
@@ -21,27 +21,88 @@ def get_images_page(domain, query, **parameters):
 			type: string
 			info: search engine to which the query is requested
 
-		query:
+		path:
 			type: string
-			info: concept to ask the search engine
+			info: relative path within the domain
 
 		parameters:
-			type: keyword arguments
+			type: dict
 			info: any additional keyword arguments required by the search engine
 
 	Returns:
 	----------
+		url:
+			type: string
+			info: link to a specific web page
+	"""
+
+	url = 'https://' + domain + path
+
+	if parameters is None:
+		url += '?'
+
+		for key, value in parameters.items():
+			url += key + '=' + value.replace(' ', '+') + '&'
+
+	return url
+
+
+
+
+def get_page(url):
+
+	""" Request a specific url content and returns its html
+
+	Arguments:
+	----------
+		url:
+			type: string
+			info: link to a specific web page
+
+	Returns:
+	----------
+		page:
+			type: BeautifulSoup object
+			info: html of the page in soup format
+	"""
+
+	response = requests.get(url)
+	page = BeautifulSoup(response.text, 'lxml')
+
+	return page
+
+
+
+
+def get_next_page(page, class_name):
+
+	""" Obtain the url from the html page to jump into the next results page
+
+	Arguments:
+	----------
 		page:
 			type: string (html format)
 			info: html of the page containing the images
+
+		class_name:
+			type: string
+			info: name of the div containing the link to the next page
+
+	Returns:
+	----------
+		url:
+			type: string
+			info: link to the next results page
 	"""
 
-	# TODO
+	urls = page.findAll('a', {'class': class_name})
+
+	return urls[-1]['href']
 
 
 
 
-def get_images_urls(page):
+def get_images_src(page):
 
 	""" Yields each image source link from the html page (Generator)
 
@@ -51,6 +112,10 @@ def get_images_urls(page):
 			type: string (html format)
 			info: html of the page containing the images
 
+		class_name:
+			type: string
+			info: name of the pictures class (defined by search engine)
+
 	Yields:
 	----------
 		source:
@@ -58,7 +123,10 @@ def get_images_urls(page):
 			info: source link of an image
 	"""
 
-	# TODO
+	images = page.findAll('img')
+
+	for img in images:
+		print(img['src'])
 
 
 
