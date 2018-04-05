521ba54ff84debe8dc76df205924105239e33cea
Sinclert <Sinclert@hotmail.com>
2018-04-05 20:08:04 +0200

    Renaming 'config' folder + Creating 'utils.py'

---
+++ src/utils.py
@@ -0,0 +1,184 @@
+# Created by Sinclert Pérez & Silvia Barbero
+
+
+import json
+import os
+import pickle
+
+
+project_paths = {
+	'dataset': ['resources', 'datasets'],
+	'model': ['models'],
+	'analysis_c': ['configs', 'analysis'],
+	'scraping_c': ['configs', 'scraping'],
+	'training_c': ['configs', 'training'],
+	'face_model': ['resources', 'face_models']
+}
+
+
+
+
+def check_keys(keys, data_struct, error):
+
+	""" Checks if all the keys are present in the data structure
+
+	Arguments:
+	----------
+		keys:
+			type: list
+			info: elements which must be in the data structure
+
+		data_struct:
+			type: set / dictionary
+			info: data structure to check existence
+
+		error:
+			type: string
+			info: error message to print
+	"""
+
+	if not all(k in data_struct for k in keys):
+		exit(error)
+
+
+
+
+def compute_path(file_name, file_type):
+
+	""" Builds the absolute path to the desired file given its file type
+
+	Arguments:
+	----------
+		file_name:
+			type: string
+			info: desired file name
+
+		file_type:
+			type: string
+			info: {'dataset', 'model', 'profile_p', 'profile_t', 'stopwords'}
+
+	Returns:
+	----------
+		path:
+			type: string
+			info: absolute path to the desired file
+	"""
+
+	try:
+		project_root = str(os.path.dirname(os.getcwd()))
+
+		path = [project_root]
+		path = path + project_paths[file_type]
+		path = path + [file_name]
+
+		return os.path.join(*path)
+
+	except KeyError:
+		exit('The file type "' + file_type + '" is not defined')
+
+
+
+
+def load_object(file_name, file_type):
+
+	""" Loads an object from the specified file
+
+	Arguments:
+		----------
+		file_name:
+			type: string
+			info: saved object file name
+
+		file_type:
+			type: string
+			info: used to determine the proper path
+
+	Returns:
+	----------
+		obj:
+			type: dict
+			info: dictionary containing the object information
+	"""
+
+	file_path = compute_path(file_name, file_type)
+
+	try:
+		file = open(file_path, 'rb')
+		obj = pickle.load(file)
+		file.close()
+
+		return obj
+
+	except IOError:
+		exit('The object could not be loaded from ' + file_path)
+
+
+
+
+def save_object(obj, file_name, file_type):
+
+	""" Saves an object in the specified path
+
+	Arguments:
+	----------
+		obj:
+			type: object
+			info: instance of a class that will be serialized
+
+		file_name:
+			type: string
+			info: saved object file name
+
+		file_type:
+			type: string
+			info: used to determine the proper path
+	"""
+
+	file_path = compute_path(file_name, file_type)
+
+	file_dir = file_path.replace(file_name, '')
+	os.makedirs(file_dir, exist_ok = True)
+
+	try:
+		file = open(file_path, 'wb')
+		pickle.dump(obj.__dict__, file)
+		file.close()
+
+	except IOError:
+		exit('The object could not be saved in ' + file_path)
+
+
+
+
+def read_json(file_name, file_type):
+
+	""" Reads a JSON file and returns it as a dictionary
+
+	Arguments:
+	----------
+		file_name:
+			type: string
+			info: readable file name
+
+		file_type:
+			type: string
+			info: used to determine the proper path
+
+	Returns:
+	----------
+		json_dict:
+			type: dict
+			info: dictionary containing the parsed JSON file
+	"""
+
+	file_path = compute_path(file_name, file_type)
+
+	try:
+		file = open(file_path, 'r', encoding = 'utf-8')
+		json_dict = json.load(file)
+		file.close()
+
+		return json_dict
+
+	except IOError:
+		exit('The file ' + file_name + ' cannot be opened')
